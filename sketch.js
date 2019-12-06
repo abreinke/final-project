32ccf8bd2597a212df1ae52ef5d341d67347c336
abreinke <42782067+abreinke@users.noreply.github.com>
2019-12-05 17:24:15 -0800

    sound update

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 function preload(){
 soundFormats('mp3' , 'm4a');
-mySound = loadSound('final-project/rain.m4a');
+setVolume(0.5);
+mySound = loadSoundFile('final-project/rain.m4a');
 }
 
 function setup(){
   background(220);
   createCanvas(400, 400);
-  soundFormats('mp3', 'm4a');
-  setVolume(0.5);
-  }
+}
 
 function keyTyped(){
-if (key == 'r');
+if (key == 't'){
+playSoundFile('final-project/thunder.mp3');
+}
+else if (key == 'r') {
+playSoundFile('final-project/rain.m4a')
 }
