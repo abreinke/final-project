bef30a527e3ee5d9b42d92a50763ba7d681c1f13
abreinke <42782067+abreinke@users.noreply.github.com>
2019-12-05 16:46:40 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -2,6 +2,19 @@ function setup() {
   createCanvas(400, 400);
 }
 
+function preload() {
+  soundFormats('mp3', 'm4a');
+  mySound = loadSound('final-project/rain.m4a');
+  mySound = loadSound('final-project/wind.mp4');
+  mySound = loadSound('final-project/thunder.mp4')
+  mySound = loadSound('final-project/hail.mp4');
+}
+
+function setup() {
+  mySound.setVolume(0.1);
+  mySound.play();
+}
+
 function draw() {
   background(220);
 }
