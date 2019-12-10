261567e84e2dcc5ff9a8e9ed06263b1dd86524e0
abreinke <42782067+abreinke@users.noreply.github.com>
2019-12-09 20:20:44 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,19 +1,45 @@
+let mySound;
+let thunderSound;
+let hailSound;
+let windSound;
+let soundVolume;
 function preload(){
 soundFormats('mp3' , 'm4a');
-setVolume(0.5);
-mySound = loadSoundFile('final-project/rain.m4a');
+mySound = loadSound('rain.m4a');
+thunderSound = loadSound('thunder.mp3');
+hailSound = loadSound('hail.mp3');
+windSound = loadSound('wind.mp3');
 }
 
 function setup(){
   background(220);
   createCanvas(400, 400);
+  soundVolume = 0.5
+  mySound.setVolume(soundVolume);
+  thunderSound.setVolume(soundVolume);
+  hailSound.setVolume(soundVolume);
+  windSound.setVolume(soundVolume);
 }
 
 function keyTyped(){
-if (key == 't'){
-playSoundFile('final-project/thunder.mp3');
+if (key == 'r'){
+mySound.play();
 }
-else if (key == 'r') {
-playSoundFile('final-project/rain.m4a')
+else if (key == 't') {
+thunderSound.play();
+}
+else if (key == 'h') {
+hailSound.play();
+}
+else if (key == 'w') {
+windSound.play();
+}
+else if (key == 'a'){
+soundVolume = soundVolume + 0.1;
+mySound.setVolume(soundVolume);
+}
+else if (key == 'd') {
+soundVolume = soundVolume - 0.1;
+mySound.setVolume(soundVolume);
 }
 }
